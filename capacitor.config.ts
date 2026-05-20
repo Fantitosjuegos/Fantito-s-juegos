eba022732c9823b6a0e539872c0916235086bce6
mayssa <mayssa.ghabarou@medtech.tn>
2026-05-20 22:36:14 +0100

    feat: initialize Capacitor, add Android platform

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.fantitosjuegos.fun',
+  appName: "Fantito's Juegos",
+  webDir: 'dist'
+};
+
+export default config;
